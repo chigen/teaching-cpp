f8b7a69c19de115b2fad7e57a4325cd739afadf6
chigen <1173279688@qq.com>
2024-03-26 17:50:36 +0900

    after lesson 05

--- main.cpp
+++ main.cpp
@@ -18,7 +18,10 @@ int main()
     // int num = 0;
     // cin >> num;
 
-    // if (num >= 0) cout << num;
+    // if (num >= 0) {
+    //     cout << num;
+    //     num ++;
+    // }
 
     // if(num >= 0){
     //     cout << "num is >= 0" << endl;
@@ -50,23 +53,29 @@ int main()
     // }
 
     // while
-    vector<int> vec{1,2,3,4,6,7,8};
+    // vector<int> vec{1,2,3,4,6,7,8};
+
     // int vecLen = vec.size();
     // int index = 0;
-    // while(index<vecLen){
+    // while(index<vecLen)
+    // {
     //     if(index%2==1){
     //         break;
     //     }
     //     cout << "current index: " << index << endl;
     //     cout << "value: " << vec[index] << endl;
+        
+        
     //     index++;
     //     // break;
     // }
 
     // // for
-    // for(int i=0,j=1; i<vecLen; ++i){
+    // for(int i=0; i<vecLen;){
     //     cout << "current index: " << i << endl;
     //     cout << "value: " << vec[i] << endl;
+        
+    //     ++i;
     // }
 
     // int i=0;
@@ -103,15 +112,15 @@ int main()
     //     num *= 2;
     // }
 
-    // for(auto num : vec){
+    // for(int num : vec){
     //     num *= 2;
     // }
 
     // for (int i=0;i<vec.size(); ++i){
     //     vec[i] *= 2;
     // }
-
-    // for(auto &num : vec){
+        // declaration: expression
+    // for(int &num : vec){
     //     num *= 2;
     // }
     // for(auto &num : vec){
@@ -120,19 +129,21 @@ int main()
 
     // string
     // 定义和初始化
-    string s1;
-    string s2(s1); // or string s2 = s1;
-    string s3 = "value"; // string s3("value")
-    string s4 = {"list initialize"};
-    string s5(5, 'a'); //初始化为由连续5个字符c组成的串
+    // string s1;
+    // string s2(s1); // or string s2 = s1;
+    // string s3 = "value"; // string s3("value")
+    // string s4 = {"list initialize"};
+    // string s5(5, 'a'); //初始化为由连续5个字符c组成的串
 
-    auto len = s5.size();
-    bool empty = s1.empty();
+    // auto len = s5.size();
+    // bool empty = s1.empty();
 
-    string s6 = s3 + ' ' + s4 + " byebye";
+    // cout << s4[2];
 
-    string temp = s4.substr(5,4);
-    cout << "sub string: " << temp << endl;
+    // string s6 = s3 + ' ' + s4 + " byebye";
+
+    // string temp = s4.substr(5,4);
+    // cout << "sub string: " << temp << endl;
 
     // int i[3];
     // int j[3] = i[3]; //error
@@ -147,6 +158,15 @@ int main()
 
     // 容器
     // 类模版 
+    // vector<double> vec2;
+    // vector<double> vec3 = vec2; // vec3(vec2);
+    // vector<double> vec4{4,3,3,6,6,7};
+    // vector<double> vec5(5);
+    // vector<string> vecStr(4, "abc");
+
+    // vec4.push_back(3.2);
+    // vec4.push_back(4.5);
+    // vec4.push_back(5.5);
 
     //初始化
 
@@ -160,6 +180,14 @@ int main()
     // int i3[3] = {2, 4};
     // int i4[] = {2, 3, 4, 5, 1010};
     // char str[3] = {'c' , 'b', 'd'};
+
+    // cout << i << endl;
+    // for(int index=0;index<5;++index){
+    //     cout << i4[index] << " ";
+    // }
+    // for(int* ip=i4, count=0;count<=5;count++, ip++){
+    //     cout << *ip << " ";
+    // }
 
     // // for (int j=0; j<3; j++){
     // //     cout << i[j];
@@ -199,6 +227,46 @@ int main()
     //         cout<< *q << endl;
     //     }
     // }
+
+    vector<int> vec{1,3,4,5,6};
+    // auto it = vec.begin();
+    // vector<int>::iterator it2 = vec.begin();
+
+    // for(auto it=vec.begin(); it!=vec.end(); ++it){
+    //     cout << *it << endl;
+    // }
+
+
+    int i1 = 1, i2 = 3;
+    if(i1 == 1){
+        cout << "i1 is 1" << endl;;
+    }
+    // if(i1 = 2){
+    //     cout << "give i1 a new value, 2" << endl;
+    // }
+    if(i1 != 2){
+        cout << "i1 is not 2" << endl;
+    }
+    if(i1 < 5){
+        cout << "i1 is less than 5" << endl;
+    }
+    if (i1>=1){
+        cout << "i1 is greater or equal to 1"<< endl;
+    }
+
+    if (i1 == 1 && i2 == 2){
+        cout << "and" << endl;
+    }
+    if (i1 == 1 || i2 == 2){
+        cout << "or" << endl;
+    }
+
+
+    vector<string> text{"hello", "world", "end", "", "hi", " again"};
+    for(auto it=text.begin(); it!=text.end() && !(*it).empty(); ++it){
+        cout << *it << " ";
+    }
+    cout << "haha";
 
     return 0;
 }
